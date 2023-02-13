118b84c72303a60b14db081546546366172687db
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-02-13 21:23:13 +0000

    add movies index page

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
 $(document).ready(function(){
-    $('#form-group').submit('click', function(e){
-        performSearch();
+    $('#searchForm').on('submit', function(e){
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
     })
 })
 
-function performSearch(e){
-    var request;
-    e.preventDefault();
-
-    request = $.ajax({
-        url: "https://imdb8.p.rapidapi.com/auto-complete?q=",
-        method: "GET",
-        data: {
-            q: $('#movieId').val(),
-            addid: "c01cfa9b42mshb8aa517229d08bfp10c509jsn9a92779a9164",
-            X_RapidAPI_Host: "imdb8.p.rapidapi.com"}
-    }),
+function getMovies(searchText){
     
-    request.done(function (response) {
-        console.log(response);
-    });
 }
